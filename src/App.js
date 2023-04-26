aaad32acf552cd217b95a670f14a3db130f41104
Jay Singh Solanki <jaysingh.solanki@citiustech.com>
2023-04-26 15:58:28 +0530

    PMS-0001 Created login screen

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import './App.css';
+import "./App.css";
 import Header from "./NavigationBar/Header.js";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-
+import Login from "./view/components/Login/Login.js";
 
 function App() {
   return (
     <>
-     <Header/>
-   
-     </>
+      <Router>
+        <div className="app-container">
+          <Routes>
+            <Route path="/" element={<Header />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
---
+++ src/view/components/Login/Login.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Login.scss";
+
+export default function Login() {
+  return (
+    <div className="login-container">
+      <div className="background-image"></div>
+
+      <div className="form-container">
+        <div className="heading">
+          <h2>Login</h2>
+        </div>
+        <form>
+          <div class="txt_field">
+            <input type="text" name="username" placeholder="Username" />
+          </div>
+          <br />
+          <div className="txt_field">
+            <input type="password" name="Password" placeholder="Password" />
+          </div>
+          <br />
+          <div className="pass">Forget Password ?</div>
+          <input type="submit" value="Login" />
+        </form>
+      </div>
+    </div>
+  );
+}
