b3ea251ef9c8d340738c7d74f3ada1bc88943118
Jay Singh Solanki <jaysingh.solanki@citiustech.com>
2023-05-09 19:49:20 +0530

    PMS-0008 Authentication using Auth0

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Header from "./view/components/Header/Header.js";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./view/components/Login/Login.js";
 import UserTable from "./view/components/Users/UserTable.js";
+import Login from "./view/components/Login/Login.js";
 
 function App() {
   return (
@@ -9,7 +9,6 @@ function App() {
       <Router>
         <div className="app-container">
           <Routes>
-            {/* <Route path="/" element={<Header />} /> */}
             <Route
               path="/"
               element={
@@ -19,7 +18,6 @@ function App() {
                 </>
               }
             />
-
             <Route path="/login" element={<Login />} />
           </Routes>
         </div>
