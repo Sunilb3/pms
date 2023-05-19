168f7cca3f6e4971b0bdbc0a40fe0df87dcd172c
PraveenH18 <PraveenH@citiustech.com>
2023-05-19 12:57:11 +0530

    PMS-22-API-Connectivity-AXIOS, connected backend with frontend using saga and axios

--- src/view/store/store.js
+++ src/view/store/store.js
@@ -1,6 +1,12 @@
-import { createStore } from 'redux';
-import usersReducer from './userReducer';
+import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
+import rootSaga from "./sagas";
+import rootReducer from "./rootReducer";
 
-const store = createStore(usersReducer);
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
